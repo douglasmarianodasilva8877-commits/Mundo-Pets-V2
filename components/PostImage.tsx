49cdbdfed8b912be28c845e7d36020b799d32581
Douglas Mariano da Silva <douglasmarianodasilva8877@gmail.com>
2025-11-13 19:59:48 -0300

    fix: PostImage component

--- components/PostImage.tsx
+++ components/PostImage.tsx
@@ -1,9 +1,19 @@
-{post.image && (
-  <div className="mt-3 flex justify-center">
-    <img
-      src={post.image}
-      alt="Imagem do post"
-      className="max-h-[400px] w-auto rounded-2xl object-cover"
-    />
-  </div>
-)}
+interface PostImageProps {
+  post: {
+    image?: string;
+  };
+}
+
+export default function PostImage({ post }: PostImageProps) {
+  if (!post?.image) return null;
+
+  return (
+    <div className="mt-3 flex justify-center">
+      <img
+        src={post.image}
+        alt="Post Image"
+        className="max-h-[500px] rounded-lg object-cover"
+      />
+    </div>
+  );
+}
